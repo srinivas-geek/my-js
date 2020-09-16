c01f6ca0d57585205556c30da1dfa0ad60ec09f1
Srinivas Erumalla <serumalla@brierley.com>
2020-09-16 12:08:52 +0530

    added useful info

---
+++ interview.js
@@ -0,0 +1,37 @@
+
+/*var a = ['dog', 'cat', 'man']
+a[100] = 'fox'
+console.log(a.length);
+-------output--------
+length:- 101
+*/
+
+/*
+"use strict"
+function logThis(){
+  this.desc = 'logger'
+  this.test = "test"
+  console.log(this);
+}
+new logThis();
+--------output------------
+logThis {desc: "logger", test: "test"}
+*/
+
+/*
+const x = 6 % 2;
+console.log(x);
+const y = x ? 'One': 'Two';
+console.log(y);
+------------output---------------
+x = 0
+y = Two
+*/
+
+/*
+console.log([3] === ['3']); // false
+console.log(3 != '3');//false
+console.log(3 == '3');//true
+console.log(3 === '3');//false
+*/
+
